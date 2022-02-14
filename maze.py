ce94e4a6fd4ada4a4c7894673863d2e26aa11c05
Tamas Nagy <tamasn.nagy004@gmail.com>
2022-02-14 22:26:39 +0100

    Added Kruskal's algo to maze.py

--- maze.py
+++ maze.py
@@ -16,6 +16,8 @@ COLOUR_YELLOW = [255, 211, 67]
 COLOUR_YELLOW2 = [204, 156, 0]
 COLOUR_GREEN = [202, 224, 12]
 COLOUR_GREEN2 = [160, 178, 9]
+COLOUR_RED = [250, 72, 27]  # [247, 54, 5]
+COLOUR_RED2 = [198, 44, 5]
 COLOUR_GRAY = [40, 40, 40]
 
 
@@ -55,6 +57,8 @@ class MazeBuilder(ABC):
         self.width = width
         self.height = height
         self._init_maze()
+        self._in_cells: list[tuple[int, int]] = []
+        self.build_steps: list[list[list[list[int, int, int]]]] = [copy.deepcopy(self._maze)]
 
     @property
     def width(self):
@@ -95,6 +99,38 @@ class MazeBuilder(ABC):
     def _init_maze(self) -> None:
         self._maze = [[COLOUR_GRAY] * self.width for _ in range(self.height)]
 
+    def _is_carved(self, xxx, yyy):
+        if (xxx, yyy) in self._in_cells:
+            return True
+        return False
+
+    def _mark_cell(self, cx, cy):
+        self._maze[cy][cx] = COLOUR_WHITE2
+
+    def _save_cell(self, xxx, yyy):
+        self._mark_cell(xxx, yyy)
+        self._in_cells.append((xxx, yyy))
+
+    def _mark_wall(self, wx, wy):
+        self._maze[wy][wx] = COLOUR_GRAY
+
+    def _add_head_and_tail(self, c: Cell) -> None:
+        while True:
+            x, y = ((self.width - 1) * c, randrange(1, self.height - 1))
+            if self._is_carved(x + 1 - 2 * c, y):
+                self._mark_cell(x, y)
+                self._save_next_anim_frame()
+                break
+
+    def _add_maze_start_point(self):
+        self._add_head_and_tail(Cell.START)
+
+    def _add_maze_end_point(self):
+        self._add_head_and_tail(Cell.END)
+
+    def _save_next_anim_frame(self):
+        self.build_steps.append(copy.deepcopy(self._maze))
+
     @abstractmethod
     def build_maze(self):
         pass
@@ -106,14 +142,8 @@ class RandomisedPrimsMazeBuilder(MazeBuilder):
         self._in_cells: list[tuple[int, int]] = []
         self._frontier_cells: list[tuple[int, int]] = []
         self._paths_to_next_cell: list[tuple[int, int]] = []
-        self.build_steps: list[list[list[list[int, int, int]]]] = [copy.deepcopy(self._maze)]
 
     def build_maze(self) -> None:
-        def is_carved(xxx, yyy):
-            if (xxx, yyy) in self._in_cells:
-                return True
-            return False
-
         def is_near_right_edge(pos):
             return pos == self.width - 2
 
@@ -131,27 +161,17 @@ class RandomisedPrimsMazeBuilder(MazeBuilder):
                 return True
             return False
 
-        def save_frontier(xxx, yyy, delta_x, delta_y):
+        def mark_frontier(xxx, yyy, delta_x, delta_y):
             if not is_marked(xxx + 2*delta_x, yyy + 2*delta_y):
                 self._frontier_cells.append((xxx + 2*delta_x, yyy + 2*delta_y))
                 self._maze[yyy + 2*delta_y][xxx + 2*delta_x] = COLOUR_YELLOW
             self._maze[yyy + delta_y][xxx + delta_x] = COLOUR_YELLOW2
 
-        def mark_cell(cx, cy):
-            self._maze[cy][cx] = COLOUR_WHITE2
-
-        def save_cell(xxx, yyy):
-            mark_cell(xxx, yyy)
-            self._in_cells.append((xxx, yyy))
-
-        def save_next_anim_frame():
-            self.build_steps.append(copy.deepcopy(self._maze))
-
-        def mark_new_frontiers(fx, fy):
-            if not is_near_right_edge(fx) and not is_carved(fx + 2, fy): save_frontier(fx, fy, 1, 0)
-            if not is_near_left_edge(fx) and not is_carved(fx - 2, fy): save_frontier(fx, fy, -1, 0)
-            if not is_near_bottom_edge(fy) and not is_carved(fx, fy + 2): save_frontier(fx, fy, 0, 1)
-            if not is_near_top_edge(fy) and not is_carved(fx, fy - 2): save_frontier(fx, fy, 0, -1)
+        def save_new_frontiers(fx, fy):
+            if not is_near_right_edge(fx) and not self._is_carved(fx + 2, fy): mark_frontier(fx, fy, 1, 0)
+            if not is_near_left_edge(fx) and not self._is_carved(fx - 2, fy): mark_frontier(fx, fy, -1, 0)
+            if not is_near_bottom_edge(fy) and not self._is_carved(fx, fy + 2): mark_frontier(fx, fy, 0, 1)
+            if not is_near_top_edge(fy) and not self._is_carved(fx, fy - 2): mark_frontier(fx, fy, 0, -1)
 
         def choose_a_random_element(list_: list[tuple[int, int]]) -> tuple[int, int]:
             shuffle(list_)
@@ -161,43 +181,29 @@ class RandomisedPrimsMazeBuilder(MazeBuilder):
             return choose_a_random_element(self._frontier_cells)
 
         def get_neighbours_of_next_cell(xx, yy):
-            if not is_near_right_edge(xx) and is_carved(xx + 2, yy): self._paths_to_next_cell.append((xx + 1, yy))
-            if not is_near_left_edge(xx) and is_carved(xx - 2, yy): self._paths_to_next_cell.append((xx - 1, yy))
-            if not is_near_bottom_edge(yy) and is_carved(xx, yy + 2): self._paths_to_next_cell.append((xx, yy + 1))
-            if not is_near_top_edge(yy) and is_carved(xx, yy - 2): self._paths_to_next_cell.append((xx, yy - 1))
+            if not is_near_right_edge(xx) and self._is_carved(xx + 2, yy): self._paths_to_next_cell.append((xx + 1, yy))
+            if not is_near_left_edge(xx) and self._is_carved(xx - 2, yy): self._paths_to_next_cell.append((xx - 1, yy))
+            if not is_near_bottom_edge(yy) and self._is_carved(xx, yy + 2): self._paths_to_next_cell.append((xx, yy + 1))
+            if not is_near_top_edge(yy) and self._is_carved(xx, yy - 2): self._paths_to_next_cell.append((xx, yy - 1))
 
         def choose_a_neighbour() -> tuple[int, int]:
             return choose_a_random_element(self._paths_to_next_cell)
 
         def add_path_to_neighbour(px: int, py: int) -> None:
-            mark_cell(px, py)
+            self._mark_cell(px, py)
 
         def solidify_walls():
             while self._paths_to_next_cell:
                 path_x, path_y = self._paths_to_next_cell.pop()
-                self._maze[path_y][path_x] = COLOUR_GRAY
-
-        def add_head_and_tail(c: Cell) -> None:
-            while True:
-                x, y = ((self.width - 1) * c, randrange(1, self.height - 1))
-                if is_carved(x + 1 - 2*c, y):
-                    mark_cell(x, y)
-                    save_next_anim_frame()
-                    break
-
-        def add_maze_start_point():
-            add_head_and_tail(Cell.START)
-
-        def add_maze_end_point():
-            add_head_and_tail(Cell.END)
+                self._mark_wall(path_x, path_y)
 
         def carve_passage(xx: int, yy: int) -> None:
             while True:
-                save_cell(xx, yy)
-                save_next_anim_frame()
+                self._save_cell(xx, yy)
+                self._save_next_anim_frame()
 
-                mark_new_frontiers(xx, yy)
-                save_next_anim_frame()
+                save_new_frontiers(xx, yy)
+                self._save_next_anim_frame()
 
                 if not self._frontier_cells:
                     break
@@ -214,9 +220,105 @@ class RandomisedPrimsMazeBuilder(MazeBuilder):
         x, y = (randrange(1, self.width - 1, 2), randrange(1, self.height - 1, 2))
         carve_passage(x, y)
 
-        add_maze_start_point()
-        add_maze_end_point()
+        self._add_maze_start_point()
+        self._add_maze_end_point()
 
+
+class RandomisedKruskalsMazeBuilder(MazeBuilder):
+    def __init__(self, width: int, height: int):
+        super().__init__(width, height)
+        self._tree_sets = [{(x, y), } for y in range(1, self.height - 1, 2) for x in range(1, self.width - 1, 2)]
+        self._hor_walls = [(x, y) for y in range(1, self.height - 1, 2) for x in range(2, self.width - 2, 2)]
+        self._ver_walls = [(x, y) for y in range(2, self.height - 2, 2) for x in range(1, self.height - 1, 2)]
+        self._walls = self._hor_walls + self._ver_walls
+
+    def build_maze(self) -> None:
+        def mark_cells_at_wall(w, c1, c2):
+            self._maze[c1[1]][c1[0]] = COLOUR_RED
+            self._maze[c2[1]][c2[0]] = COLOUR_RED
+            self._maze[w[1]][w[0]] = COLOUR_RED2
+
+        def choose_wall() -> tuple[int, int]:
+            shuffle(self._walls)
+            wall_x, wall_y = self._walls.pop()
+            return wall_x, wall_y
+
+        def get_cells_near_wall(xx: int, yy: int) -> tuple[tuple[int, int], tuple[int, int]]:
+            if xx % 2 == 0:
+                c1 = (xx - 1, yy)
+                c2 = (xx + 1, yy)
+            else:
+                c1 = (xx, yy - 1)
+                c2 = (xx, yy + 1)
+
+            mark_cells_at_wall((xx, yy), c1, c2)
+            return c1, c2
+
+        def cells_in_same_tree(c1, c2):
+            for set_ in self._tree_sets:
+                if c1 in set_:
+                    if c2 in set_:
+                        return True
+                    else:
+                        return False
+
+        def join_tree_sets(c1, c2):
+            for i, set_ in enumerate(self._tree_sets):
+                if c1 in set_:
+                    i1 = i
+                    break
+
+            for i, set_ in enumerate(self._tree_sets):
+                if c2 in set_:
+                    i2 = i
+                    break
+
+            set_ = self._tree_sets[i2]
+            self._tree_sets[i1] = self._tree_sets[i1] | set_
+            self._tree_sets.remove(set_)
+
+        def join_cells(c1, c2):
+            self._save_cell(c1[0], c1[1])
+            self._save_cell(c2[0], c2[1])
+            if c1[0] == c2[0]:
+                self._save_cell(c1[0], c1[1] + 1)
+            else:
+                self._save_cell(c1[0] + 1, c1[1])
+
+            join_tree_sets(c1, c2)
+
+        def mark_cannot_remove(xx, yy):
+            self._maze[yy][xx] = COLOUR_YELLOW
+            self._save_next_anim_frame()
+            self._mark_wall(xx, yy)
+
+        def solidify_wall(c1, c2):
+            self._save_cell(c1[0], c1[1])
+            self._save_cell(c2[0], c2[1])
+            if c1[0] == c2[0]:
+                mark_cannot_remove(c1[0], c1[1] + 1)
+            else:
+                mark_cannot_remove(c1[0] + 1, c1[1])
+
+        def carve_passage() -> None:
+            while True:
+                if self._walls:
+                    x, y = choose_wall()
+                else:
+                    break
+
+                cell_1, cell_2 = get_cells_near_wall(x, y)
+                self._save_next_anim_frame()
+
+                if not cells_in_same_tree(cell_1, cell_2):
+                    join_cells(cell_1, cell_2)
+                else:
+                    solidify_wall(cell_1, cell_2)
+
+        carve_passage()
+
+        self._add_maze_start_point()
+        self._add_maze_end_point()
 
 if __name__ == '__main__':
     size = input("Enter maze size as \"width\"x\"height\": ")
@@ -226,4 +328,10 @@ if __name__ == '__main__':
     prims_maze.build_maze()
 
     maze_to_img(prims_maze.build_steps[-1], 10).show()
-    maze_to_animation(prims_maze.build_steps, 'maze_big10', 10, 120)
+    maze_to_animation(prims_maze.build_steps, 'maze_prims', 10, 120)
+
+    kruskals_maze = RandomisedKruskalsMazeBuilder(int(w), int(h))
+    kruskals_maze.build_maze()
+
+    maze_to_img(kruskals_maze.build_steps[-1], 10).show()
+    maze_to_animation(kruskals_maze.build_steps, 'maze_kruskals', 10, 120)
